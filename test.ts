f163f550fe94b6ee5142092e6a8213d848445b20
Dvdplate <94102213+Dvdplate@users.noreply.github.com>
2024-03-07 12:14:50 +0200

    removed main and updated test

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+for (let i = 0; i < 10; i++) {
+    basic.pause(100)
+    apprenticeBot.ledSetColor(kBit.enums.colorSelection.Red)
+    basic.pause(100)
+    apprenticeBot.ledSetColor(kBit.enums.colorSelection.Blue)
+    basic.pause(100)
+    apprenticeBot.ledSetColor(kBit.enums.colorSelection.Green)
+    basic.pause(100)
+    apprenticeBot.ledSetColor(kBit.enums.colorSelection.White)
+}
